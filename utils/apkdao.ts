1693d813418b41a88f20177a38c9a6d55b0192cb
Kurisu <makise_kurisuu@outlook.jp>
2024-04-01 19:19:07 +0800

    🐱 feat(*): 调整搜索条件

--- utils/apkdao.ts
+++ utils/apkdao.ts
@@ -16,7 +16,6 @@ export async function queryByKw(kw = "", limit = 10) {
   const results = await db.selectFrom("apk_info").selectAll()
     .where((eb) =>
       eb.or([
-        eb("fid", "=", kw),
         eb("app_name", "like", `%${kw}%`),
       ])
     ).orderBy("upload_at asc")
